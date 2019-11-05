a683e6d1b3f8dce7d87678b8527fa6fb41f94cd9
KevinDelcourt <delcokevgmail.com>
2019-11-05 08:26:25 +0100

    fin distance

--- mcs_dtw/distance.py
+++ mcs_dtw/distance.py
@@ -1,6 +1,7 @@
 """
     Fonctions de distance utilisées dans le TP
 """
+import numpy
 
 
 def euclidienne(num_a, num_b):
@@ -17,3 +18,22 @@ def entre_sequences_adn(char_a, char_b):
     if char_a == char_b:
         return 0
     return 1
+
+
+def entre_signaux_audio_exercice(char_a, char_b):
+    """
+        Distance entre deux signaux audio (cf TD1)
+    """
+    if char_a == char_b:
+        return 0
+    if char_a == 'V' and char_b != 'U' or char_b == 'V' and char_a != 'U':
+        return 2
+    return 1
+
+
+def entre_fenetres_audio(vec_a, vec_b):
+    """
+        Distance entre deux fenetres audios (vecteur de longueur 12 d'une mfcc)
+    """
+    vec_c = vec_b - vec_a
+    return numpy.linalg.norm(vec_c)
